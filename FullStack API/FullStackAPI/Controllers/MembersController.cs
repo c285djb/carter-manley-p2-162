d71448579cf4a44b4548e11fbdbaa10c865b0268
c285djb <manleyc1@gmatc.matc.edu>
2023-11-30 10:04:03 -0600

    Second Project 2 Commit

---
+++ FullStack API/FullStackAPI/Controllers/MembersController.cs
@@ -0,0 +1,38 @@
+﻿using FullStackAPI.Data;
+using FullStackAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MembersController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+        public MembersController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMembers()
+        {
+            var members = await _fullStackDbContext.Members.ToListAsync();
+
+            return Ok(members);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMember([FromBody] Member memberRequest)
+        {
+            memberRequest.Id = Guid.NewGuid();
+
+            await _fullStackDbContext.Members.AddAsync(memberRequest);
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(memberRequest);
+        }
+
+    }
+}
---
+++ FullStack API/FullStackAPI/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿namespace FullStackAPI.Models
+{
+    public class Member
+    {
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public long Phone { get; set; }
+
+        public long Points{ get; set; }
+
+        public string Department { get; set; }
+    }
+}
